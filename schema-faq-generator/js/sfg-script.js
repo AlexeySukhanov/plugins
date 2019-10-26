52ae25ddde23ea0577356043091ffbf2c270805b
AlexeySukhanov <webdevforbiz@gmail.com>
2019-10-27 02:11:17 +0300

    addNewItem()

--- schema-faq-generator/js/sfg-script.js
+++ schema-faq-generator/js/sfg-script.js
@@ -1,32 +1,30 @@
-jQuery(document).ready(function(){
-    // console.log('script connected');
-    // alert('lol');
+jQuery( document ).ready( function(){
 
-let items = [];
-let start =
-`<script type="application/ld+json">
-{
-    "@context": "https://schema.org",
-    "@type": "FAQPage",
-    "mainEntity": [`;
-let end =
-`   ]
-}`;
-let result = '';
-    jQuery(document).on('input', '.sfg_input', function () {
+    function createSchema() {
+        // prepare start end etc..
+        let items = [];
+        let start =
+            `<script type="application/ld+json">
+        {
+            "@context": "https://schema.org",
+            "@type": "FAQPage",
+            "mainEntity": [`;
+        let end =`\t\t]\n}\n</script>`;
+        let result = '';
 
-        jQuery('.sfg_name_input').each(function(index, element){
+        // add name
+        jQuery('.sfg_name_input').each( function(index, element){
             items[index] = { name: jQuery(element).val() } ;
         });
 
-        jQuery('.sfg_text_input').each(function(index, element){
+        // add text
+        jQuery('.sfg_text_input').each( function(index, element){
             items[index].text =  jQuery(element).val() ;
         });
 
-        let result = '';
-
+        // generate items list
         for ( let i = 0; i < items.length; i++ ) {
-            let coma = i == items.length - 1 ? '' : ',';
+            let coma = i == items.length - 1 ? '\n' : ',';
             console.log(i);
             result += `
                             {
@@ -36,19 +34,46 @@ let result = '';
                                     "@type": "Answer",
                                     "text": "` + items[i].text + `"
                                 }
-                            }
-                            ` + coma;
+                            }` + coma;
         }
+        result = start + result + end;
 
-
-
-        sfg_output.innerHTML = start + result + end;
-
+        sfg_output.innerHTML = result;
         console.log( items.valueOf() );
+
+    }
+
+    function addNewItem() {
+        let newItem = `
+             <li class="sfg_item">
+                <div class="sfg_draggable">draggable</div>
+                <div class="sfg_item_content">
+                    <input placeholder="question" type="text" class="sfg_input sfg_name_input">
+                    <textarea placeholder="answer" class="sfg_input sfg_text_input"></textarea>
+                </div>
+                <button class="sfg_delete_item" type="button">delete item</button>
+             </li>
+        `;
+        jQuery('.sfg_faq_list').append( newItem );
+        console.log('element created');
+    }
+    jQuery('#sfg_add_new').on( 'click', function() {
+        addNewItem();
+        createSchema();
+    });
+
+
+
+    // init generate
+        createSchema();
+    // on input generate
+    jQuery(document).on( 'input', '.sfg_input', function () {
+        createSchema();
     });
 
 
 });
+
 
 // sfg_input.oninput = function() {
 //     sfg_output.innerHTML = sfg_input.value;
